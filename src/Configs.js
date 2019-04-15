febd0d00751fab7f88a9a1f2369d7353b7afc08d
aneudy1702 <elrey1702@gmail.com>
2019-04-15 19:59:23 -0400

    added app configs

---
+++ src/Configs.js
@@ -0,0 +1,6 @@
+const GITHUB_APP_ID = '28947';
+const GITHUB_APP_NAME = 'be-social-on-github';
+const GITHUB_CLIENT_ID = 'Iv1.354068e6bfce89cd';
+const GITHUB_CLIENT_SECRET = '35612b48a662aa9448f100f7c6f6b815a2866727';
+const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize';
+const GITHUB_REDIRECT_URI = 'http://localhost:3000/';
